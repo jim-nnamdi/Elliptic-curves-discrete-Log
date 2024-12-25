cb4e03e5c39218d8db14a1acab40e7acb282d6c3
Jim <jimsamuel50@gmail.com>
2024-12-25 22:47:19 +0100

    modular addition for values

---
+++ mod_add.asm
@@ -0,0 +1,16 @@
+section .text
+    global _mod_add
+
+_mod_add:
+    ;; set the variables for a & b
+    ;; rdi = a rsi = b and rdx = p
+    ;; (a + b) mod p
+
+    add rdi, rsi    ; rdi =  a + b
+    cmp rdi, rdx    ; compare rdi with modulus in rdx
+    jl .done        ; if (a + b) < p skip subtraction
+    sub rdi, rdx    ; if (a + b) >= p sub rdi from rdx
+
+.done:
+    mov rax, rdi    ; return result of op (a + b) % p to rax
+    ret   
